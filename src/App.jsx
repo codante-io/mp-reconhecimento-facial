72289c9c01e96eb60b34992ae60fc3e040dae087
Jonathan <jonathankarlinski57@gmail.com>
2023-11-22 20:42:27 -0300

    feat: Capturando valores das expressões

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import WebCam from './components/WebCam';
 import { AppContainer } from './styles/AppStyle';
-import loadModels from './utils/faceApi';
+import loadModels, { faceDetection } from './utils/faceApi';
 
 function App() {
   const canvasRef = useRef(null);
   const webcamRef = useRef(null);
+  const [expressionsData, setExpressionsData] = useState({});
+
+  const handleExpressions = (expressions) => {
+    setExpressionsData(expressions);
+  };
 
   useEffect(() => {
-    loadModels(webcamRef, canvasRef);
+    loadModels(webcamRef, canvasRef)
+      .then(() => {
+        faceDetection(webcamRef, canvasRef, handleExpressions);
+      })
+      .catch((error) => {
+        console.log('Erro ao carregar modelos:', error.message);
+      });
   }, []);
+
+  const determineEmotion = () => {
+    const { happy, sad } = expressionsData;
+
+    if (happy > 0.7) {
+      return 'Você parece feliz!';
+    } if (sad > 0.1) {
+      return 'Você parece triste!';
+    }
+    return 'Você parece normal.';
+  };
+
+  const emotionMessage = determineEmotion();
 
   return (
     <div className="container">
@@ -24,6 +48,7 @@ function App() {
           <canvas ref={canvasRef} width="600" height="450" />
         </section>
         <section className="emoji-container">
+          <p>{emotionMessage}</p>
           {/* CHAME O SEU COMPONENTE EMOJI AQUI */}
         </section>
       </AppContainer>
--- src/utils/faceApi.js
+++ src/utils/faceApi.js
@@ -1,6 +1,6 @@
 import * as faceApi from 'face-api.js';
 
-export const faceDetection = (webcamRef, canvasRef) => {
+export const faceDetection = (webcamRef, canvasRef, expressionCallback) => {
   const { video } = webcamRef.current;
   const canvas = canvasRef.current;
   const displaySize = {
@@ -16,22 +16,26 @@ export const faceDetection = (webcamRef, canvasRef) => {
       canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
       faceApi.draw.drawDetections(canvas, resizedDetections);
       faceApi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      if (expressionCallback) {
+        const expressions = resizedDetections[0]?.expressions || {};
+        expressionCallback(expressions);
+      }
     } catch (error) {
       console.log('Erro na detecção de rosto:', error.message);
     }
   }, 100);
 };
 
-const loadModels = (webcamRef, canvasRef) => {
-  Promise.all([
-    faceApi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceApi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceApi.nets.faceExpressionNet.loadFromUri('/models'),
-  ])
-    .then(() => faceDetection(webcamRef, canvasRef))
-    .catch((error) => {
-      console.log('Erro ao carregar modelos:', error.message);
-    });
-};
+const handleExpressions = (expressions) => expressions;
+
+const loadModels = (webcamRef, canvasRef) => Promise.all([
+  faceApi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceApi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceApi.nets.faceExpressionNet.loadFromUri('/models'),
+])
+  .then(() => faceDetection(webcamRef, canvasRef, handleExpressions))
+  .catch((error) => {
+    console.log('Erro ao carregar modelos:', error.message);
+  });
 
 export default loadModels;
